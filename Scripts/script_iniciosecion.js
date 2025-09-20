fa8bebc55fa402aac8e67074a39b99ad48afcfcc
Juan Pablo Landa <juanpi9landa@gmail.com>
2025-09-20 20:13:52 -0300

    Refactor iniciosesion.html and add script_iniciosecion.js for form handling and client registration

---
+++ Scripts/script_iniciosecion.js
@@ -0,0 +1,25 @@
+const supabaseUrl = 'https://qxbkfmvugutmggqwxhrb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4YmtmbXZ1Z3V0bWdncXd4aHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNTEzMDEsImV4cCI6MjA3MzgyNzMwMX0.Qsx0XpQaSgt2dKUaLs8GvMmH8Qt6Dp_TQM25a_WOa8E'
+const { createClient } = supabase
+const client = createClient(supabaseUrl, supabaseKey)
+const form = document.getElementById('inicioform')
+const mensaje = document.getElementById('mensaje')
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const Nombre = document.getElementById('nombre').value
+    const Mail = document.getElementById('correo').value
+    const Contra = document.getElementById('contraseña').value
+    const { data, error } = await client
+    .from('Clientes')
+    .insert([{ Mail, Nombre, Contra }])
+
+    if (error) {
+    console.error('Error al registrar:', error.message)
+    mensaje.innerText = '❌ Error al registrar: ' + error.message
+    } else {
+    console.log('Cliente registrado correctamente:', data)
+    mensaje.innerText = '✅ Cliente registrado correctamente'
+    form.reset()
+    }
+})
