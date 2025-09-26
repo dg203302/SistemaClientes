c309f764b489352760443cf0fe59b7945ba67335
Diego <168243435+dg203302@users.noreply.github.com>
2025-09-26 20:25:52 -0300

    Refactor main page layout, enhance header and sidebar structure, and add content sections for points and promotions

--- Scripts/Scripts_pagina_principal/script_pagina_principal.js
+++ Scripts/Scripts_pagina_principal/script_pagina_principal.js
@@ -1,5 +1,4 @@
-
-// esto para la plantilla de pagina_principal_puntos
+async 
 const supabaseUrl = 'https://qxbkfmvugutmggqwxhrb.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4YmtmbXZ1Z3V0bWdncXd4aHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNTEzMDEsImV4cCI6MjA3MzgyNzMwMX0.Qsx0XpQaSgt2dKUaLs8GvMmH8Qt6Dp_TQM25a_WOa8E'
 const { createClient } = supabase
