7d2884358dbec6ff079f13c5269fd517fd1176a1
Diego <168243435+dg203302@users.noreply.github.com>
2025-09-26 19:40:41 -0300

    Refactor password recovery flow, update form handling, and enhance security question validation

--- Scripts/Scripts_recuperar_contrasenia/script_recuperar_contrasena.js
+++ Scripts/Scripts_recuperar_contrasenia/script_recuperar_contrasena.js
@@ -4,7 +4,7 @@ const { createClient } = supabase
 const client = createClient(supabaseUrl, supabaseKey)
 document.getElementById("recuperarForm").addEventListener("submit",async (e) =>{
     e.preventDefault();
-    let tel = document.getElementById("telef").value;
+    let tel = JSON.stringify(document.getElementById("telef").value);
     const { data, error } = await client
         .from('Clientes')
         .select('Telef')
@@ -13,23 +13,8 @@ document.getElementById("recuperarForm").addEventListener("submit",async (e) =>{
     if (error){
         const valor = 4;
         window.location.href = `/Templates/Template_informe/Informe.html?informe=${encodeURIComponent(error.message)}&valor=${encodeURIComponent(valor)}`;
-    }else {
-        let codigo_gen = ''
-        for (let i = 0; i < 4; i++) {
-            codigo_gen += Math.floor(Math.random() * 10);
-        }
-        let codigo_recu = parseInt(codigo_gen,10)
-        const{data, error} = await client
-            .from('Codigos_recuperacion')
-            .upsert({Telef:tel, codigo_rec: codigo_recu}, {onConflict: 'Telef'})
-            .single();
-        }
-        if(error){
-            const valor = 4;
-            window.location.href = `/Templates/Template_informe/Informe.html?informe=${encodeURIComponent(error.message)}&valor=${encodeURIComponent(valor)}`;
-        }else{
-            sessionStorage.setItem("telefono_usuario_recu", tel)
-            //aca poner el envio del mensaje con el codigo!
-            window.location.href = "/Templates/Templates_recuperar_contrasenia/Preguntas_cambiar_contraseña.html"
-        }
+    }else{
+        sessionStorage.setItem("telefono_usuario_recu", (JSON.stringify(tel)))
+        window.location.href = "/Templates/Templates_recuperar_contrasenia/Preguntas_cambiar_contraseña.html"
+    }
 })
---
+++ Scripts/Scripts_recuperar_contrasenia/script_validar_preguntas.js
@@ -0,0 +1,54 @@
+import { encriptar} from "../encriptado.js"
+const supabaseUrl = 'https://qxbkfmvugutmggqwxhrb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4YmtmbXZ1Z3V0bWdncXd4aHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNTEzMDEsImV4cCI6MjA3MzgyNzMwMX0.Qsx0XpQaSgt2dKUaLs8GvMmH8Qt6Dp_TQM25a_WOa8E'
+const { createClient } = supabase
+const client = createClient(supabaseUrl, supabaseKey)
+var Resp_1_bd = ''
+var Resp_2_bd = ''
+var Resp_3_bd = ''
+var tele_recu = sessionStorage.getItem("telefono_usuario_recu");
+window.onload = async () => {
+    const {data,error} = await client
+    .from('Clientes')
+    .select('Resp_1, Resp_2, Resp_3')
+    .eq('Telef',tele_recu)
+    .single()
+    if (error){
+        const valor = 5;
+        window.location.href = `/Templates/Template_informe/Informe.html?informe=${encodeURIComponent(error.message)}&valor=${encodeURIComponent(valor)}`;
+    }
+    else{
+        Resp_1_bd=data.Resp_1
+        Resp_2_bd=data.Resp_2
+        Resp_3_bd=data.Resp_3
+    }
+}
+
+document.getElementById("Preguntas_seg").addEventListener('submit', function(e){
+    e.preventDefault();
+    if (document.getElementById("Pregunta_1").value === ''){
+        alert("debe ingresar la respuesta a la primer pregunta!")
+        return
+    }
+    else if (document.getElementById("Pregunta_2").value === ''){
+        alert("debe ingresar la respuesta a la segunda pregunta!")
+        return
+    }
+    else if (document.getElementById("Pregunta_3").value === ''){
+        alert("debe ingresar la respuesta a la tercer pregunta!")
+        return
+    }
+    else if ((document.getElementById("Pregunta_1").value === '')&&(document.getElementById("Pregunta_2").value === '')&&(document.getElementById("Pregunta_3").value === '')){
+        alert("debe responder las preguntas!")
+        return
+    }
+
+    if ((encriptar(document.getElementById("Pregunta_1").value).equals(Resp_1_bd))&&(encriptar(document.getElementById("Pregunta_2").value).equals(Resp_2_bd))&&(encriptar(document.getElementById("Pregunta_3").value).equals(Resp_3_bd))){
+        console.log("los codigos coinciden \n codigo de la bd: "+codigo_BD+"\n codigo ingresado: "+ codi_ing)
+        window.location.href = "/Templates/Templates_recuperar_contrasenia/actualizar_contra.html"
+    }
+    else{
+        const valor = 5;
+        window.location.href = `/Templates/Template_informe/Informe.html?informe=${encodeURIComponent("mal codigo")}&valor=${encodeURIComponent(valor)}`;
+    }
+})
