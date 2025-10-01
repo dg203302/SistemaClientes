34ffe1c31d964f6b14468f999d5d736695142fd3
Diego <168243435+dg203302@users.noreply.github.com>
2025-10-01 19:22:30 -0300

    feat: Add script for user points and integrate Supabase client

---
+++ Scripts/Scripts_pagina_principal/script_premios_puntos.js
@@ -0,0 +1,11 @@
+const supabaseUrl = 'https://qxbkfmvugutmggqwxhrb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4YmtmbXZ1Z3V0bWdncXd4aHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgyNTEzMDEsImV4cCI6MjA3MzgyNzMwMX0.Qsx0XpQaSgt2dKUaLs8GvMmH8Qt6Dp_TQM25a_WOa8E'
+const { createClient } = supabase
+const client = createClient(supabaseUrl, supabaseKey)
+const usuario_l = JSON.parse(localStorage.getItem("usuario_loggeado"))
+
+window.onload = async function() {
+    let cantidad_puntos = document.getElementById("puntos-usuario");
+    cantidad_puntos.textContent = usuario_l.puntos_u
+}
+console.log(usuario_l.nombre_u, usuario_l.puntos_u, usuario_l.tele_u, usuario_l.f_creacion_u)
